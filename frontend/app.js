a5700322d5c93f36372b9faa7e13b738ab77eaaa
Dagan Martinez <git@dagans.dev>
2023-04-18 19:33:19 -0400

    Progress

--- frontend/app.js
+++ frontend/app.js
@@ -11,7 +11,7 @@ sse.addEventListener("message", (e) => {
 });
 
 let color = {red: 255, green:0, blue:0};
-let radius = 50;
+let radius = 10;
 
 function form_update_from_rect(x, y, r) {
     return {
@@ -55,11 +55,13 @@ function apply_update(update) {
     ctx.fillRect(start_x, start_y, end_x - start_x, end_y - start_y);
 }
 
-canvas.addEventListener('click', function(e) {
+canvas.addEventListener('mousedown', function(e) {
     const mousex = size * (e.offsetX / canvas.clientWidth)
     const mousey = size * (e.offsetY / canvas.clientHeight)
     const update = form_update_from_rect(mousex, mousey, radius);
+    ctx.fillStyle = "pink";
     console.log(update);
     send_update(update);
-    //ctx.fillRect(mousex-radius, mousey-radius, 2*radius, 2*radius);
+    ctx.fillStyle = "blue";
+    ctx.fillRect(mousex-radius, mousey-radius, 2*radius, 2*radius);
 });
